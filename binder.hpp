22f0225ffb4dc9becf045ee27f271675cf129ac8
scottidler <scottidler@gmail.com>
2014-07-10 23:02:54 -0700

    uncommented code and fixed all typos, allowing compilation... -sai

--- binder.hpp
+++ binder.hpp
@@ -43,39 +43,42 @@ namespace z2h {
     using LedPtr = TAst (TParser::*)(TAst left, Token<TAst> *token);
 
     template <typename TAst, typename TParser>
-    using ScanPtr = TAst (TParser::*)(Symbol<TAst> *symbol, const std::string &source, size_t lbp);
+    using ScanPtr = long (TParser::*)(Symbol<TAst> *symbol, const std::string &source, size_t lbp);
 
     template <typename TAst, typename TParser>
     struct Binder {
-/*
+
         StdFunc<TAst> BindStd(nullptr_t method) {
             return nullptr;
         }
+
         StdFunc<TAst> BindStd(StdPtr<TAst, TParser> method) {
-            return std::bind(method, this);
+            return std::bind(method, static_cast<TParser *>(this));
         }
+
         NudFunc<TAst> BindNud(nullptr_t method) {
             return nullptr;
         }
-*/
+
         NudFunc<TAst> BindNud(NudPtr<TAst, TParser> method) {
-            return std::bind(method, this, _1);
+            return std::bind(method, static_cast<TParser *>(this), _1);
         }
-/*
+
         LedFunc<TAst> BindLed(nullptr_t method) {
             return nullptr;
         }
+
         LedFunc<TAst> BindLed(LedPtr<TAst, TParser> method) {
-            return std::bind(method, this, _1, _2);
+            return std::bind(method, static_cast<TParser *>(this), _1, _2);
         }
+/*
         ScanFunc<TAst> BindScan(nullptr_t method) {
             return nullptr;
         }
-        ScanFunc<TAst> BindScan(ScanPtr<TAst, TParser> method) {
-            return std::bind(method, this, _1, _2, _3);
-        }
 */
-        
+        ScanFunc<TAst> BindScan(ScanPtr<TAst, TParser> method) {
+            return std::bind(method, static_cast<TParser *>(this), _1, _2, _3);
+        }
     };
 
 }
