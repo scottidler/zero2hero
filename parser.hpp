6611132e6fa702b87655807ee4224dca2b1ec036
scottidler <scottidler@gmail.com>
2014-07-26 12:07:18 -0700

    removed dead code, changed starting index to -1, and removed debug printing code... -sai

--- parser.hpp
+++ parser.hpp
@@ -54,7 +54,7 @@ namespace z2h {
             : source("")
             , position(0)
             , tokens({})
-            , index(0) {
+            , index(-1) {
         }
 
         // Symbols must be defined by the inheriting parser
@@ -80,7 +80,7 @@ namespace z2h {
                 }
                 return new Token<TAst>(match, source, position, end - position, skip);
             }
-            std::cout << "failed to match anything" << std::endl;
+            //std::cout << "failed to match anything" << std::endl;
             return new Token<TAst>(eof, source, position, 0, true); //eof
         }
 
@@ -120,44 +120,7 @@ namespace z2h {
             std::string text((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
             return text;
         }
-/*
-        Token<TAst> * Take(Token<TAst> *token) {
-            tokens.push_back(token);
-            return token;
-        }
 
-        Token<TAst> * Emit() {
-            if (index < tokens.size()) {
-                return tokens[index++];
-            }
-            return Scan();
-        }
-
-        Token<TAst> * Scan() {
-
-            size_t end = position;
-            Symbol<TAst> *match = nullptr;
-            bool skip = false;
-            if (position < source.length()) {
-                for (auto symbol : Symbols()) {
-                    long delta = symbol->Scan(symbol, source.substr(position, source.length() - position), position);
-                    if (delta) {
-                        if (abs(delta) > 0 || (match != nullptr && symbol->lbp > match->lbp && position + abs(delta) == end)) {
-                            match = symbol;
-                            end = position + abs(delta);
-                            skip = delta < 0;
-                        }
-                    }
-                }
-                if (position == end) {
-                    throw ParserException("Parser::Scan: invalid symbol");
-                }
-                return new Token<TAst>(match, source, position, end - position, skip);
-            }
-            std::cout << "failed to match anything" << std::endl;
-            return new Token<TAst>(); //eof
-        }
-*/
         TAst ParseFile(const std::string &filename) {
             auto source = Load(filename);
             return Parse(source);
@@ -167,22 +130,19 @@ namespace z2h {
             this->source = source;
             return Expression();
         }
+
         TAst Expression(size_t rbp = 0) {
 
             auto *curr = Consume();
-            std::cout << "curr: " << *curr << std::endl;
             if (nullptr == curr->symbol->Nud)
                 throw ParserException("unexpected: nullptr == Nud");
 
             TAst left = curr->symbol->Nud(curr);
 
             auto *next = LookAhead(1);
-            std::cout << "next: " << *next << std::endl;
             while (rbp < next->symbol->lbp) {
                 next = Consume();
-                std::cout << "inner next: " << *next << std::endl;
                 left = next->symbol->Led(left, next);
-                std::cout << "left: " << left->Print() << std::endl;
             }
 
             return left;
@@ -198,37 +158,7 @@ namespace z2h {
             Consume(1, "EndOfStatement expected!");
             return ast;
         }
-/*
-        Token<TAst> * LookAhead(size_t distance) {
-            if (distance == 0)
-                return tokens[index];
 
-            while(distance > tokens.size() - index) {
-                auto *token = Scan();
-                if (token->skip) {
-                    position += token->length;
-                    continue;
-                }
-                Take(Scan());
-            }
-
-            return Emit();
-        }
-
-        Token<TAst> * Consume() {
-            LookAhead(1);
-            auto curr = Emit();
-            position += curr->length;
-            return curr;
-        }
-
-        Token<TAst> * Consume(const size_t &expected, const std::string &message) {
-            auto token = Consume();
-            if (token->symbol->type != expected)
-                throw ParserException(message);
-            return token;
-        }
-*/
         size_t Line() {
             return 0;
         }
